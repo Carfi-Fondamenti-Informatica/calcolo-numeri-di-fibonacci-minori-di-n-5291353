a2da1104d8ec144a92873b81bb0f140df43e28eb
Francesco <91691097+5291353@users.noreply.github.com>
2021-11-10 21:35:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0,a=0, b=1,c=0;
+    cout<<"Inserire numero:"<<endl;
+    cin>> n;
+        cout<<"1"<<endl;
+            for (int i = 1; i < n; i++) {
+                c = a + b;
+                if(c<=n) {
+                    cout << c << endl;
+                }else{
+                    return 0;
+                }
+                a = b;
+                b = c;
+            }
+
+    return 0;
 }
